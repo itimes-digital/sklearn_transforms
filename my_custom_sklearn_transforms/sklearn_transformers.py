a81d34604e90752325013bf47ebe0666126393ee
Mr. Anderson <47112285+itimes-digital@users.noreply.github.com>
2020-08-20 13:40:31 -0300

    ajuste de transformação para dataframe

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+import pandas as pd
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -13,8 +13,8 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
-    
+        return pd.DataFrame.from_records(data=data.drop(labels=self.columns, axis='columns'))
+        
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropNA(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
@@ -27,4 +27,4 @@ class DropNA(BaseEstimator, TransformerMixin):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
         # Retornamos um novo dataframe sem as linhas indesejadas
-        return data.dropna(subset = self.columns)
+        return pd.DataFrame.from_records(data=data.dropna(subset = self.columns))
