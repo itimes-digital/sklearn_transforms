923314a337aa336dd8f7488af97c044ee09552d4
Mr. Anderson <47112285+itimes-digital@users.noreply.github.com>
2020-08-20 13:08:29 -0300

    Inserção de drop de NA
    maratona ibm

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,17 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+    
+# All sklearn Transforms must have the `transform` and `fit` methods
+class DropNA(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        # Retornamos um novo dataframe sem as linhas indesejadas
+        return data.dropna(subset = self.columns)
